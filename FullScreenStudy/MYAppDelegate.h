77d354c09eb5a3a17b0667e9f7cea6c3c9cb3080
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2014-08-30 01:34:52 +0900

    Initial Commit

---
+++ FullScreenStudy/MYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MYAppDelegate.h
+//  FullScreenStudy
+//
+//  Created by adachi yuichi on 2014/08/30.
+//  Copyright (c) 2014年 yad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FullScreenStudy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FullScreenStudy
+//
+//  Created by adachi yuichi on 2014/08/30.
+//  Copyright (c) 2014年 yad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MYAppDelegate class]));
+    }
+}
